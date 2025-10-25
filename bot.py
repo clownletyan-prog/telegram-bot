c2269eb9e2e1b4ed0ff6ab5d54f11312608a801b
clownletyan-prog <clownletyan@gmail.com>
2025-10-26 02:52:51 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,12 @@
 - безопасное экранирование MarkdownV2
 - логирование в bot.log
 """
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('0.0.0.0', 10000))
 
+# Ваш существующий код ниже...
+from telegram.ext import Application, CommandHandler
 import os
 import json
 import random
